34884e35f11eab054e36ee4e8543f4e4ed1f8989
Craftingmod <craftingmod@tarks.net>
2018-05-26 17:39:42 +0900

    Check permission granted

--- app/src/main/java/net/tarks/craftingmod/chuno/MainActivity.java
+++ app/src/main/java/net/tarks/craftingmod/chuno/MainActivity.java
@@ -1,7 +1,16 @@
 package net.tarks.craftingmod.chuno;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.Spanned;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +18,36 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String url = "https://github.com/craftingmod/Chuno/blob/master/README.md";
+        final boolean granted_write = this.checkCallingOrSelfPermission("android.permission.WRITE_SECURE_SETTINGS") == PackageManager.PERMISSION_GRANTED;
+        if(!granted_write) {
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.it_chuno_nopermTitle))
+                    .setMessage(getTextWithURL(getString(R.string.it_chuno_nopermContent),url))
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                            finish();
+                        }
+                    })
+                    .show();
+        }
+    }
+    @SuppressWarnings("deprecation")
+    private Spanned getTextWithURL(String text, String url){
+        text = text.replace("$url","<a href=\'$url\'>$url</a>").replaceAll("\\$url",url);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return Html.fromHtml(text,Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            return Html.fromHtml(text);
+        }
+    }
+    private void dialog(String title,String content,DialogInterface.OnClickListener clicked){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setPositiveButton(android.R.string.ok, clicked)
+                .setMessage(content)
+                .show();
     }
 }
